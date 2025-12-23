ba6beb1898cb9c5a04fcb99bf9edece03c4c580c
MariaGarbuzova05 <mariagarbuzova378@gmail.com>
2025-12-23 12:57:05 +0300

    commit

--- src/main/java/com/example/demo/Task2Application.java
+++ src/main/java/com/example/demo/Task2Application.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// !!! Для пользователя: логин: user, пароль: user
+// !!! Для администратора: логин: admin, пароль: admin
+
 @SpringBootApplication
 public class Task2Application {
 
